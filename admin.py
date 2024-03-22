c363571e60811745e7d4dfe58b249dd258686092
LLIJIYAPNIK <kas4wot@yandex.ru>
2024-03-22 18:43:23 +0300

    Очертания админки. Не требуется дизайн)) Мелкие исправления

---
+++ admin.py
@@ -0,0 +1,31 @@
+from flask_admin import Admin, BaseView, AdminIndexView, expose
+from flask_admin.contrib.sqla import ModelView
+from models import User, Posts, db, PostReactions, Gyms
+
+
+class DashboardView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        return self.render('dashboard_index.html')
+
+
+class StatsView(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('stat_admin.html')
+
+
+class UserView(ModelView):
+    column_list = User.__table__.columns.keys()
+
+
+class PostsView(ModelView):
+    column_list = Posts.__table__.columns.keys()
+
+
+class GymsView(ModelView):
+    column_list = Gyms.__table__.columns.keys()
+
+
+class PostReactionsView(ModelView):
+    column_list = PostReactions.__table__.columns.keys()
